33f97032582fa07c53de200b30de854c27f0d78e
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-10-28 18:43:05 +0700

    Implement a review post to show in Review-Hub page

--- kuhub/views.py
+++ kuhub/views.py
@@ -1,32 +1,40 @@
+from kuhub.models import Post
 from django.views import generic
-from kuhub.models import *
-
 
 # Create your views here.
 
+
 class ReviewHubView(generic.ListView):
     template_name = 'kuhub/review.html'
+    context_object_name = 'posts_list'
 
     def get_queryset(self):
-        return Post.objects.order_by('-post_date')
+        """Return recently published review posts."""
+        return Post.objects.filter(tag_id=1).order_by('-post_date')
 
 
 class SummaryHubView(generic.ListView):
     template_name = 'kuhub/summary.html'
+    context_object_name = 'latest_post_list'
 
     def get_queryset(self):
+        """Return recently published summary posts."""
         return Post.objects.order_by('-post_date')
 
 
 class TricksHubView(generic.ListView):
     template_name = 'kuhub/tricks.html'
+    context_object_name = 'latest_post_list'
 
     def get_queryset(self):
+        """Return recently published trick posts."""
         return Post.objects.order_by('-post_date')
 
 
 class EncouragementView(generic.ListView):
     template_name = 'kuhub/encourage.html'
+    context_object_name = 'latest_post_list'
 
     def get_queryset(self):
+        """Return recently published encourage posts."""
         return Post.objects.order_by('-post_date')
