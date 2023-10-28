81aa611e4b3312f6466d8d8fb03a351b6b05ed45
Napasorn TEVARUT <napasorn.te@ku.th>
2023-10-28 15:53:55 +0700

    add login signup template

--- isp_project/settings.py
+++ isp_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,11 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'django.contrib.sites', #new
+    "kuhub",
     'allauth', #new
     'allauth.account', #new
     'allauth.socialaccount', #new
     'allauth.socialaccount.providers.google', #new
-    "kuhub",
 ]
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
@@ -79,7 +79,8 @@ ROOT_URLCONF = "isp_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates'),
+    ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
