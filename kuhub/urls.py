6ca88be2d2c1bd8133589f43c99c1b071ab66aa1
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-10-29 13:49:39 +0700

    Set start path redirect to Review-Hub

--- kuhub/urls.py
+++ kuhub/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from kuhub import views
+from django.views.generic import RedirectView
 
 
 app_name = "kuhub"
 urlpatterns = [
+    path('', RedirectView.as_view(url="review-hub/")),
     path('review-hub/', views.ReviewHubView.as_view(), name='review'),
     path('summary-hub/', views.SummaryHubView.as_view(), name='summary'),
     path('tricks-hub/', views.TricksHubView.as_view(), name='tricks'),
