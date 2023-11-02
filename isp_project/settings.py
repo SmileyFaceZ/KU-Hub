0c608a61a9b09c36b5b7bc0df203330afb91a7be
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-11-03 00:23:47 +0700

    Implement Summary-Hub page finished

--- isp_project/settings.py
+++ isp_project/settings.py
@@ -142,9 +142,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'kuhub/static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
