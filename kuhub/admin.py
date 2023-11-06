e59a53ef2c58a205d7aaff5512d75c43262cbced
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-11-06 14:33:54 +0700

    Dump subject data in json file format

--- kuhub/admin.py
+++ kuhub/admin.py
@@ -1,7 +1,7 @@
 """Import admin class from django"""
 from django.contrib import admin
 from kuhub.models import \
-    UserFollower, Post, PostComments, Tags, PostDownload, PostReport
+    UserFollower, Post, PostComments, Tags, PostDownload, PostReport, Subject
 
 # Register your models here.
 
@@ -11,3 +11,4 @@ admin.site.register(PostComments)
 admin.site.register(Tags)
 admin.site.register(PostDownload)
 admin.site.register(PostReport)
+admin.site.register(Subject)
