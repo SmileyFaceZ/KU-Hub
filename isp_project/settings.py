009f457b431457854990ee893c7e51a2a31e564b
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-11-13 23:20:03 +0700

    Adjust a DATABASES with postgres url instead

--- isp_project/settings.py
+++ isp_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config, Csv
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,14 +110,11 @@ WSGI_APPLICATION = "isp_project.wsgi.application"
 # To use Neon with Django, you have to create a Project on Neon and specify the project connection settings in your settings.py in the same way as for standalone Postgres.
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': config("DATABASE_NAME", default="kuhubdb"),
-    'USER': config("DATABASE_USER", default="KU-Hub"),
-    'PASSWORD': config("DATABASE_PASSWORD", default="password"),
-    'HOST': config("HOST", default="localhost"),
-    'PORT': config("PORT", default="5432"),
-  }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default='postgres-url'),
+        conn_max_age=500,
+        conn_health_checks=True
+    )
 }
 
 # Password validation
