40a6cebe502f203b74805e8965806187c4083717
Worawalan C <113490970+woraamy@users.noreply.github.com>
2023-11-23 12:18:55 +0700

    Add allowed host

--- isp_project/settings.py
+++ isp_project/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = config("SECRET_KEY", default="secret-key-value-without-quotes")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=False)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv(), default=['ku-hub.onrender.com'])
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    cast=Csv(),
+    default="127.0.0.1,localhost,ku-hub.onrender.com",
+)
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
