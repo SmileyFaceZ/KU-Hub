deb1f0bcad759bce9b8809d6d41e830fe356f065
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-11-23 23:49:38 +0700

    Add filebase CDN

---
+++ kuhub/static/kuhub/js/filebase-store.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBwV28WvuaYFGPGhW3pGv3nhZZq_gdVpcY",
+    authDomain: "ku-hub-76621.firebaseapp.com",
+    projectId: "ku-hub-76621",
+    storageBucket: "ku-hub-76621.appspot.com",
+    messagingSenderId: "493628516213",
+    appId: "1:493628516213:web:b0af6e5f779ed07896953c",
+    measurementId: "G-QDN5PB162J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
