5fd38c8e62b9de2b9d57c29611be78fb737c8f8b
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-11-24 21:04:26 +0700

    Move a re-path in the last url

--- kuhub/urls.py
+++ kuhub/urls.py
@@ -7,7 +7,6 @@ from django.urls import re_path
 
 app_name = "kuhub"
 urlpatterns = [
-    re_path(r'^.*/$', RedirectView.as_view(url='/kuhub/review-hub/', permanent=True), name='redirect_to_review_hub'),
     path('', RedirectView.as_view(url="review-hub/")),
     path('review-hub/', views.ReviewHubView.as_view(), name='review'),
     path('summary-hub/', views.SummaryHubView.as_view(), name='summary'),
@@ -37,4 +36,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.edit_post, name='edit_post'),
     path('report/<int:pk>/', views.report_post, name='report_post'),
+    re_path(r'^.*/$', RedirectView.as_view(url='/kuhub/review-hub/', permanent=True), name='redirect_to_review_hub'),
 ]
