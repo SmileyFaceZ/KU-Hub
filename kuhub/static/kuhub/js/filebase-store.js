92da3f8aaeafc0d157b77542b4ebd6fce1778977
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-11-24 16:46:00 +0700

    Add a javascript to upload into firebase server

--- kuhub/static/kuhub/js/filebase-store.js
+++ kuhub/static/kuhub/js/filebase-store.js
@@ -1,14 +1,25 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-storage.js";
+import { firebaseConfig } from "./firebase-config.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBwV28WvuaYFGPGhW3pGv3nhZZq_gdVpcY",
-    authDomain: "ku-hub-76621.firebaseapp.com",
-    projectId: "ku-hub-76621",
-    storageBucket: "ku-hub-76621.appspot.com",
-    messagingSenderId: "493628516213",
-    appId: "1:493628516213:web:b0af6e5f779ed07896953c",
-    measurementId: "G-QDN5PB162J"
-};
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+const fileUpload = document.getElementById("file-upload-input");
+fileUpload.addEventListener("change", function(event) {
+    const file = event.target.files[0];
+    console.log(file);
+    const fileref = ref(storage, file.name)
+    uploadBytes(fileref, file)
+        .then((result) => {
+            alert("File uploaded successfully!");
+    });
+});
+
+const fileref = ref(storage, file.name)
+
+fileref.getDownloadURL().then(function(url) {
+    document.getElementById("viewer").src = url;
+}).catch(function(error) {
+    console.error("Error getting download URL: ", error);
+});
