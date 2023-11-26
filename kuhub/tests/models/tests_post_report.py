482b943501183f64ffacf2481e6aa009bffedafb
Worawalan C <113490970+woraamy@users.noreply.github.com>
2023-11-26 19:19:36 +0700

    Delete unrelated comments

--- kuhub/tests/models/tests_post_report.py
+++ kuhub/tests/models/tests_post_report.py
@@ -26,19 +26,6 @@ class PostReportModelTests(TestCase):
             subject=self.subject
         )
 
-    # def test_number_of_report(self):
-    #     report_1 = PostReport(
-    #         post_id=self.post1,
-    #         report_reason="test report"
-    #     )
-    #     report_1.save()
-    #     report_2 = PostReport(
-    #         post_id=self.post1,
-    #         report_reason="test report 2"
-    #     )
-    #     report_2.save()
-    #     self.assertEqual(report_2.report_count, 1)
-
     def test_report_post_relationship(self):
         """
         post_id should establish a ForeignKey relationship with the Post model.
