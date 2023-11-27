b371fb80b22ca0f65c8be0e7945e832793d63a09
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-11-27 13:59:06 +0700

    Set logger in firebase.py

--- isp_project/settings.py
+++ isp_project/settings.py
@@ -16,6 +16,8 @@ from decouple import config, Csv
 import dj_database_url
 from kuhub import firebase
 
+firebase.initialize_firebase()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- kuhub/firebase.py
+++ kuhub/firebase.py
@@ -1,27 +1,31 @@
 import firebase_admin
 from firebase_admin import credentials
 from decouple import config
+import logging
 
-print("Initializing Firebase...")
-try:
-    firebase_config = {
-        "type": "service_account",
-        "project_id": config('FIREBASE_PROJECT_ID'),
-        "private_key_id": config('FIREBASE_PRIVATE_KEY_ID'),
-        "private_key": config('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
-        "client_email": config('FIREBASE_CLIENT_EMAIL'),
-        "client_id": config('FIREBASE_CLIENT_ID'),
-        "auth_uri": config('FIREBASE_AUTH_URI'),
-        "token_uri": config('FIREBASE_TOKEN_URI'),
-        "auth_provider_x509_cert_url": config('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
-        "client_x509_cert_url": config('FIREBASE_CLIENT_X509_CERT_URL'),
-        "universe_domain": config('FIREBASE_UNIVERSE_DOMAIN'),
-    }
+LOGGER = logging.getLogger("kuhub")
 
-    cred = credentials.Certificate(firebase_config)
-    firebase_admin.initialize_app(cred, {
-        'storageBucket': config('FIREBASE_STORAGE_BUCKET', default='appspot')
-    })
-    print("Firebase Initialized successfully")
-except Exception as e:
-    print("Error initializing Firebase:", e)
+def initialize_firebase():
+    print("Initializing Firebase...")
+    try:
+        firebase_config = {
+            "type": "service_account",
+            "project_id": config('FIREBASE_PROJECT_ID'),
+            "private_key_id": config('FIREBASE_PRIVATE_KEY_ID'),
+            "private_key": config('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+            "client_email": config('FIREBASE_CLIENT_EMAIL'),
+            "client_id": config('FIREBASE_CLIENT_ID'),
+            "auth_uri": config('FIREBASE_AUTH_URI'),
+            "token_uri": config('FIREBASE_TOKEN_URI'),
+            "auth_provider_x509_cert_url": config('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+            "client_x509_cert_url": config('FIREBASE_CLIENT_X509_CERT_URL'),
+            "universe_domain": config('FIREBASE_UNIVERSE_DOMAIN'),
+        }
+
+        cred = credentials.Certificate(firebase_config)
+        firebase_admin.initialize_app(cred, {
+            'storageBucket': config('FIREBASE_STORAGE_BUCKET', default='appspot')
+        })
+        LOGGER.info("Firebase Initialized successfully")
+    except Exception as e:
+        LOGGER.error("Error initializing Firebase:", e)
