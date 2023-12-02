55f861ef29cc3e48811bc7e4471ac618eff96bad
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2023-12-02 12:09:36 +0700

    Change initialize view

--- kuhub/views/__init__.py
+++ kuhub/views/__init__.py
@@ -1,13 +1,19 @@
+# Group Package
+from .group.group_feature import GroupFeature
+from .group.group_view import GroupView
+from .group.group_event import GroupEventController
+from .group.group_detail_view import GroupDetail
+from .group.group_event_detail import EventDetail
+
+# Profile Package
+from .profile.profile_setting import ProfileSetting
+from .profile.profile_view import ProfileView
+
+from .base_view import ReviewHubView, TricksHubView
+from .firebase_folder import FirebaseFolder
+from .follow_view import FollowView
 from .gened_type_view import GenEdTypeListView
-from .group_view import GroupView, join, create_group
-from .post_feature_view import (
-    create_post, post_detail, edit_post, report_post, like_post, dislike_post
-)
-from .profile_view import (
-    profile_view, profile_settings, toggle_follow,
-    followers_page, following_page
-)
+from .home_view import HomePageView
+from .post_feature_view import PostFeature
 from .subject_detail_view import SubjectDetailView
 from .summary_view import SummaryHubView
-from .base_view import ReviewHubView, TricksHubView
-from .group_detail_view import GroupDetail, group_event_create, group_event_delete, add_note, delete_note
